c53ba75c465e79cb6ed4f4d3c97ebb36fdf53af9
monique2026190 <monique2026190@gmail.com>
2026-04-12 18:25:17 +0000

    Fix: Corrige a tipagem de arquivo do Multer no Controle.Usuario

--- backend/controles/Controle.Usuario.ts
+++ backend/controles/Controle.Usuario.ts
@@ -1,13 +1,12 @@
 
 import { Request, Response, NextFunction } from 'express';
-import { File } from 'multer';
 import servicoUsuario from '../ServicosBackend/Servico.Usuario.js';
 import validadorUsuario from '../validators/Validator.Estrutura.Usuario.js';
 import createControllerLogger from '../config/Log.Controles.js';
 
 // Defina uma interface que estende a interface Request e inclui a propriedade 'file'
 interface RequestWithFile extends Request {
-  file?: File;
+  file?: Express.Multer.File;
 }
 
 const logger = createControllerLogger('Controle.Usuario.ts');
